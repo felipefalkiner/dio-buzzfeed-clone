ba9dd64e48d1622318e6e33304c3e194542d2a49
felipefalkiner <felipe.magosso@gmail.com>
2023-12-23 17:56:50 -0300

    changed fedd to feed on the project

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/dio-buzzfedd-clone'),
+      dir: require('path').join(__dirname, './coverage/dio-buzzfeed-clone'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dio-buzzfedd-clone';
+  title = 'dio-buzzfeed-clone';
 }
