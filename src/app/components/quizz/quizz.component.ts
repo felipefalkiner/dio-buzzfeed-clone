2ba1df7c1c8716c172d0a6b40f468abb992ab509
felipefalkiner <felipe.magosso@gmail.com>
2023-12-23 18:47:17 -0300

    created the quizz logic

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -5,7 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './quizz.component.html',
   styleUrls: ['./quizz.component.css']
 })
+
 export class QuizzComponent implements OnInit {
+
+  title:string = "";
+
+  questions:any;
+  questionSelected:any;
+
+  answers:string[] = [];
+  answerSelected:string = "";
+
+  questionIndex:number = 0;
+  questionMaxIndex:number = 0;
+
+  finished:boolean = false;
 
   constructor() { }
 
